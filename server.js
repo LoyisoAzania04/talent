748f479417e560446e170171a830143fa84c2c72
Loyiso04 <loyisokhumalo04@gmail.com>
2024-03-28 12:35:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+console.log('testing')
+const http = require('http')
+
+const port = 808
+
+const server = http.createServer(function(req,res){
+    res.write('Hello World')
+    res.end()
+
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('something went wrong',error)
+    }else{
+        console.log('server is listening on port' + ': ' + port)
+    }
+})
